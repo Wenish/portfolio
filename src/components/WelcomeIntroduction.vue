6f6afce3d3ae78dfddd4c70c3ceed7889f4689e8
Wenish <Wenish@users.noreply.github.com>
2022-05-21 14:26:51 +0200

    add welcome introduction component

---
+++ src/components/WelcomeIntroduction.vue
@@ -0,0 +1,6 @@
+<template>
+<div>Hi There,</div>
+<div>I'm Jonas Voland</div>
+<div>I do application development and music production.</div>
+<img alt="Animate Picture of Jonas Voland with effects" src="../assets/voland.gif"/>
+</template>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,9 +1,11 @@
 <template>
 <div>Page Home</div>
+<WelcomeIntroduction></WelcomeIntroduction>
 <ProjectsList></ProjectsList>
 </template>
 <script lang="ts" setup>
 import { defineAsyncComponent } from 'vue';
 
+const WelcomeIntroduction = defineAsyncComponent(() => import('../components/WelcomeIntroduction.vue'))
 const ProjectsList = defineAsyncComponent(() => import('../components/ProjectsList.vue'))
 </script>
