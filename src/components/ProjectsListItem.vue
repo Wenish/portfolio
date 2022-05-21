0cd24fbfe0f53771b31e38896557892e73734480
Wenish <Wenish@users.noreply.github.com>
2022-05-21 18:26:03 +0200

    adding more data

--- src/components/ProjectsListItem.vue
+++ src/components/ProjectsListItem.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="py-4 grid place-items-center">
-    <div class="max-w-4xl grid grid-cols-1 place-items-center md:grid-cols-2 px-4">
+    <div class="w-full max-w-4xl grid grid-cols-1 place-items-center md:grid-cols-2 px-4">
         <div class="content font-light">
             <div class="text-2xl pb-2">{{ title }}</div>
-            <div class="py-1 text-base"><i class="fa-regular fa-calendar"></i> {{ dateStart }} - {{ dateEnd }}</div>
+            <div class="py-1 text-base"><i class="fa-regular fa-calendar"></i> {{ formattedDateStart }} - {{ formattedDateEnd }}</div>
             <div class="text-lg">{{ description }}</div>
             <div class="flex flex-wrap gap-2 py-4">
-                <div v-for="(tech, index) in techStack" :key="index" class="bg-orange-400 rounded-full px-2 font-medium">{{ tech }}</div>
+                <div v-for="(tech, index) in techStack.sort((a, b) => a.localeCompare(b))" :key="index" class="bg-orange-400 rounded-full px-2 font-medium">{{ tech }}</div>
             </div>
         </div>
         <div>
@@ -17,6 +17,8 @@
 </template>
 <script lang="ts" setup>
 import { PropType } from 'vue';
+import { DateTime } from "luxon";
+import { computed } from '@vue/reactivity';
 
 const props = defineProps({
     title: {
@@ -43,6 +45,18 @@ const props = defineProps({
         type: Array as PropType<Array<string>>,
         default: ['https://i.imgur.com/gQUlPMN.jpeg']
     },
+})
+
+const formatDate = (date: string) => {
+    return DateTime.fromFormat(date, 'dd.MM.yyyy').toFormat('yyyy MMMM')
+}
+
+const formattedDateStart = computed(() => {
+    return formatDate(props.dateStart)
+})
+
+const formattedDateEnd = computed(() => {
+    return formatDate(props.dateEnd)
 })
 </script>
 <style scoped>
--- src/stores/projects.store.ts
+++ src/stores/projects.store.ts
@@ -20,11 +20,11 @@ export const useProjectsStore = defineStore(
     const loadProjects = () => {
       projects.value = [
         {
-          title: 'Rooms and Ressources',
+          title: 'Swisscom Rooms and Ressources',
           description: 'Internal Room Booking System for Swisscom AG.',
           dateStart: '01.01.2019',
           dateEnd: '01.03.2020',
-          techStack: ['Node', 'React'],
+          techStack: ['JavaScript', 'Node', 'React', 'Azure DevOps'],
           images: [assetUrlRnr1]
         },
         {
@@ -32,7 +32,7 @@ export const useProjectsStore = defineStore(
           description: 'A realtime multiplayer platformer game.',
           dateStart: '01.02.2022',
           dateEnd: '01.05.2022',
-          techStack: ['Unity', 'Node', 'Colyseus', 'TypeScript', 'Vue', 'C#', 'Firebase'],
+          techStack: ['Unity', 'Node', 'Colyseus', 'TypeScript', 'Vue', 'C#', 'Firebase', 'Github', 'Heroku'],
           images: []
         },
         {
@@ -40,15 +40,55 @@ export const useProjectsStore = defineStore(
           description: 'An E-commerce platform for resalling used goods.',
           dateStart: '01.11.2021',
           dateEnd: '01.05.2022',
-          techStack: ['Node', 'TypeScript', 'Vue', 'Nestjs', 'Firebase', 'Azure', 'MongoDB'],
+          techStack: ['Node', 'TypeScript', 'Vue', 'Nestjs', 'Firebase', 'Azure', 'MongoDB', 'Azure DevOps'],
+          images: []
+        },
+        {
+          title: 'Swisscom News App',
+          description: 'Creating a internal news application with a content managment system.',
+          dateStart: '01.03.2015',
+          dateEnd: '01.03.2016',
+          techStack: ['JavaScript', 'Node', 'Knockoutjs', 'Sharepoint', 'Azure DevOps'],
+          images: []
+        },
+        {
+          title: 'Swisscom Partner Portal',
+          description: 'Creating a internal application for searching and booking personal coaches.',
+          dateStart: '01.03.2016',
+          dateEnd: '01.07.2017',
+          techStack: ['JavaScript', 'Node', 'Knockoutjs', 'Sharepoint', 'Azure DevOps'],
+          images: []
+        },
+        {
+          title: 'Swisscom Digital Post-Its',
+          description: 'A web application for a digital Post-It wall.',
+          dateStart: '01.12.2014',
+          dateEnd: '01.02.2015',
+          techStack: ['JavaScript', 'jQuery', 'Sharepoint', 'Azure DevOps'],
+          images: []
+        },
+        {
+          title: 'Swisscom Blog App',
+          description: 'A internal web application for sharing blog posts with images and text.',
+          dateStart: '01.03.2016',
+          dateEnd: '01.05.2016',
+          techStack: ['JavaScript', 'Angular', 'Node', 'Sharepoint', 'Azure DevOps'],
+          images: []
+        },
+        {
+          title: 'Swisscom Butler',
+          description: 'A windows workplace support app.',
+          dateStart: '01.08.2017',
+          dateEnd: '01.12.2018',
+          techStack: ['C#', 'UWP', 'Microsoft Bot Framework', 'Azure Functions', 'Azure DevOps'],
           images: []
         }
-      ]
+      ].sort((a, b) => (new Date(a.dateEnd) < new Date(b.dateEnd)) ? 1 : ((new Date(b.dateEnd) < new Date(a.dateEnd)) ? -1 : 0))
     }
     loadProjects()
 
     return {
-        projects
+      projects
     }
   }
 )
