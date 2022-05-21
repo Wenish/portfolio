1f42fc50c6c0cedc4d93987c60c2d4a67b1b4a5b
Wenish <Wenish@users.noreply.github.com>
2022-05-21 16:51:01 +0200

    Update TheFooter.vue

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,11 @@
 <template>
-<div class="grid justify-center py-4">
-    © 2022 Jonas Voland
-</div>
+    <div class="grid justify-center py-4">
+        <div>
+            <div class="text-xs font-light">
+                <a href="https://www.flaticon.com/free-icons/person" target="_blank" title="person icons">Person icon created by Vitaly
+                    Gorbachev - Flaticon</a>
+            </div>
+            <div class="py-4 text-lg">© 2022 Jonas Voland</div>
+        </div>
+    </div>
 </template>
