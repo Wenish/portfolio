2d03f1737ae184d330e4fb04961a7c5714e635fc
Wenish <Wenish@users.noreply.github.com>
2022-07-21 17:58:04 +0200

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: 'var(--brand)',
+        text1: 'var(--text1)',
+        text2: 'var(--text2)',
+        link: 'var(--link)',
+        surface1: 'var(--surface1)',
+        surface2: 'var(--surface2)',
+        surface3: 'var(--surface3)',
+        surface4: 'var(--surface4)',
+      },
+    },
   },
   plugins: [],
 }
