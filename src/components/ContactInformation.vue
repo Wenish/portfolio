8365aa65c0ec53a00aa51605246af3ce31dcd654
Wenish <Wenish@users.noreply.github.com>
2023-11-01 16:18:04 +0100

    Update ContactInformation.vue

--- src/components/ContactInformation.vue
+++ src/components/ContactInformation.vue
@@ -3,7 +3,7 @@
         <h2 class="text-3xl font-bold">Contact Me</h2>
         <div>via</div>
         <div class="font-mono text-xl">
-            <div>WhatsApp or Telegram <span><br />+41 79 881 75 00</span></div>
+            <div>Email<span><br />john.volo321@gmail.com</span></div>
         </div>
     </div>
 </template>
