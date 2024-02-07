f3bdf3cd2b0584bf9ed3599c4cf2aab4e5d80848
Wenish <Wenish@users.noreply.github.com>
2024-02-07 20:28:42 +0100

    Update TheHeader.vue

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,10 +2,10 @@
     <div class="px-8 py-6">
         <div class="flex gap-6 max-w-7xl mx-auto">
             <a class="text-green-700 hover:text-green-600 inline-block" href="https://open.spotify.com/artist/2UMPXGNnurwP45ae7lPdAG" target="_blank">
-                <IconSpotify class=" h-6 w-6" />
+                <IconSpotify class=" h-8 w-8" />
             </a>
             <a class="text-violet-700 hover:text-violet-600" href="https://github.com/Wenish" target="_blank">
-                <IconGithub class=" h-6 w-6" />
+                <IconGithub class=" h-8 w-8" />
             </a>
         </div>
     </div>
