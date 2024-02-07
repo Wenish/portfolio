836b6687a3ad22c629b0ec8da120d76b66db3a12
Wenish <Wenish@users.noreply.github.com>
2024-02-07 20:13:53 +0100

    Update EducationInformation.vue

--- src/components/EducationInformation.vue
+++ src/components/EducationInformation.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="max-w-4xl grid m-auto text-left font-bold px-4">
         <h2 class="text-3xl font-bold">Education</h2>
-        <div>Languages - German and Swiss German (mother tongue), English</div>
-        <div>Apprenticeship, Swisscom Bern - Computer science EFZ (August 2012 - August 2016)</div>
-        <div>Further study, Berlin - Scrum Alliance Product Owner (March 2017)</div>
-        <div>Further study, Bern - Berufsbildner (June 2020)</div>
-        <div>Further study, Bern - Leading SAFe (April 2023)</div>
-        <div>Further study, Bern - Scrum Alliance Scum Master (June 2023)</div>
-        <div>Study, TEKO Bern - Dipl. Informatiker/in HF Fachrichtung Applikation (Currently ongoing)</div>
         <div>Study, FFHS Zürich - CAS Full-Stack Development (Currently ongoing)</div>
+        <div>Study, TEKO Bern - Dipl. Informatiker/in HF Fachrichtung Applikation (Currently ongoing)</div>
+        <div>Further study, Bern - Scrum Alliance Scum Master (June 2023)</div>
+        <div>Further study, Bern - Leading SAFe (April 2023)</div>
+        <div>Further study, Bern - Berufsbildner (June 2020)</div>
+        <div>Further study, Berlin - Scrum Alliance Product Owner (March 2017)</div>
+        <div>Apprenticeship, Swisscom Bern - Computer science EFZ (August 2012 - August 2016)</div>
+        <div>Languages - German and Swiss German (mother tongue), English</div>
     </div>
 </template>
 <script setup lang="ts"></script>
