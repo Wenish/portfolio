a25a28b2da325f36236739e9256f0cff5f9b2ee8
Wenish <Wenish@users.noreply.github.com>
2024-07-25 01:24:25 +0200

    Update EducationInformation.vue

--- src/components/EducationInformation.vue
+++ src/components/EducationInformation.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="max-w-4xl grid m-auto text-left font-bold px-4">
         <h2 class="text-3xl font-bold">Education</h2>
-        <div>Study, FFHS Zürich - CAS Full-Stack Development (Currently ongoing)</div>
         <div>Study, TEKO Bern - Advanced Federal Diploma of Higher Education in Information Technology (Currently ongoing)</div>
+        <div>Study, FFHS Zürich - CAS Full-Stack Development (Faburary 2024 - July 2024)</div>
         <div>Apprenticeship, Swisscom Bern - Computer science EFZ (August 2012 - August 2016)</div>
     </div>
 </template>
