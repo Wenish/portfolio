132c35d9688d6a9f43729be78a587623179d2cd2
Wenish <Wenish@users.noreply.github.com>
2024-12-13 12:55:48 +0100

    update education info

--- src/components/EducationInformation.vue
+++ src/components/EducationInformation.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="max-w-4xl grid m-auto text-left font-bold px-4">
         <h2 class="text-3xl font-bold">Education</h2>
-        <div>Study, TEKO Bern - Advanced Federal Diploma of Higher Education in Information Technology (Currently ongoing)</div>
+        <div>Study, TEKO Bern - Advanced Federal Diploma of Higher Education in Information Technology (October 2023 - expected 2026)</div>
+        <div>Study, FFHS Zürich - CAS Web Usability (expected 2025)</div>
         <div>Study, FFHS Zürich - CAS Full-Stack Development (February 2024 - July 2024)</div>
-        <div>Apprenticeship, Swisscom Bern - Computer science EFZ (August 2012 - August 2016)</div>
+        <div>Apprenticeship, Swisscom AG Bern - Computer science EFZ (August 2012 - August 2016)</div>
     </div>
 </template>
 <script setup lang="ts"></script>
--- src/components/WelcomeIntroduction.vue
+++ src/components/WelcomeIntroduction.vue
@@ -3,7 +3,7 @@
         <div class="text-4xl font-bold content">
             <div class="text-6xl">HI, I AM</div>
             <div class="text-4xl sm:text-5xl py-4">JONAS VOLAND</div>
-            <div class="text-xl font-light">Professional software engineer, part time music producer and located in Switzerland.</div>
+            <div class="text-xl font-light">Professional software engineer, part time hobby music producer and located in Switzerland.</div>
         </div>
         <div>
             <img alt="Animate Picture of Jonas Voland with effects" src="../assets/volandOptimized.gif"
