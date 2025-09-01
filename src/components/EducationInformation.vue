525ed3b6539f924a9cf46ac4317db3cf77bee01d
Wenish <Wenish@users.noreply.github.com>
2025-09-01 15:25:28 +0200

    Update EducationInformation.vue

--- src/components/EducationInformation.vue
+++ src/components/EducationInformation.vue
@@ -2,7 +2,8 @@
     <div class="max-w-4xl grid m-auto text-left font-bold px-4">
         <h2 class="text-3xl font-bold">Education</h2>
         <div>Study, TEKO Bern - Advanced Federal Diploma of Higher Education in Information Technology (October 2023 - expected 2026)</div>
-        <div>Study, FFHS Zürich - CAS Web Usability (expected 2025)</div>
+        <div>Study, FFHS Zürich - CAS Web- und Informationsdesign (expected December 2025)</div>
+        <div>Study, FFHS Zürich - CAS Web Usability (February 2025 - July 2025)</div>
         <div>Study, FFHS Zürich - CAS Full-Stack Development (February 2024 - July 2024)</div>
         <div>Apprenticeship, Swisscom AG Bern - Computer science EFZ (August 2012 - August 2016)</div>
     </div>
